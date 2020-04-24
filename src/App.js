f4272f2d59eabd27dde5d806d6d8b0b024ef47f8
whatsquackin@hotmail.com <whatsquackin@hotmail.com>
2020-04-24 16:46:27 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import {Component} from 'react';
 import Moment from 'moment';
 import 'moment-timezone';
-import zipToTz from'zipcode-to-timezone'
+import zipToTz from'zipcode-to-timezone';
+//import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap dist/js/bootstream.js';
 
 
 class App extends Component {
@@ -16,7 +18,8 @@ class App extends Component {
     time:'',
     date:'',
     show: false,
-   
+    option: 'imperial',
+    degree: '°F'
   }
 
   myClick =() => {
@@ -37,8 +40,28 @@ class App extends Component {
     }
   }
 
+  optionChange =(event) =>{
+    this.setState({
+      option: event.target.value ,
+    })
+    setTimeout(()=> {
+      if (this.state.option == 'imperial'){
+        this.setState({
+          degree: '°F'
+      })
+    } else {
+      this.setState({
+        degree: '°C'
+      })
+    }
+    }, 100)
+    
+
+    this.myClick();
+  }
+
   getWeather = () => {
-     fetch('https://api.openweathermap.org/data/2.5/weather?zip='+this.state.input+',us&units=imperial&appid='+this.state.apikey)
+     fetch('https://api.openweathermap.org/data/2.5/weather?zip='+this.state.input+',us&units='+this.state.option+'&appid='+this.state.apikey)
      .then((result) => {
         return result.json();
      })
@@ -48,7 +71,7 @@ class App extends Component {
         this.setState({
           city: result.name,
           weather: result.weather[0].main,
-          temperature: result.main.temp,
+          temperature: Math.round(result.main.temp),
      })
     })
   }
@@ -70,12 +93,41 @@ class App extends Component {
         <input id ={"input"} type= "text" placeholder="Enter Zip" onKeyUp ={(keypress) =>{this.myKeyPress(keypress)}} />
         <button id ={"inputBtn"} onClick={()=>{this.myClick()}}>Submit</button>
       </div>
+
+  <div>
+    
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="imperial"
+              checked={this.state.option === "imperial"}
+              onChange={this.optionChange}
+            />
+            Fahrenheit (°F)
+          </label>
+        </div>
+        
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="metric"
+              checked={this.state.option === "metric"}
+              onChange={this.optionChange}
+            />
+            Celsius (°C)
+          </label>
+        </div>
+      
+    </div>
+
       <div style = {{ display: (this.state.show ? "block" : "none")}}>
           <h4>
           City: {this.state.city}<br></br>
           Time: {this.state.time}<br></br>
           Weather is {this.state.weather} <br></br>
-          with a temperature of {this.state.temperature}<br></br>
+          with a temperature of {this.state.temperature} {this.state.degree}<br></br>
           </h4>
       </div>
     </>  
