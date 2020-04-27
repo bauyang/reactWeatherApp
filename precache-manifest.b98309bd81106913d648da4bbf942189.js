988f889565f19be7da3c861013bfcc089153af3b
whatsquackin@hotmail.com <whatsquackin@hotmail.com>
2020-04-26 21:05:16 -0400

    Updates

---
+++ precache-manifest.b98309bd81106913d648da4bbf942189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a074674b0cd2ece89303f83094617e8",
+    "url": "/reactWeatherApp/index.html"
+  },
+  {
+    "revision": "4bf37ef3e303fa5a87ae",
+    "url": "/reactWeatherApp/static/css/main.f3a4e5cb.chunk.css"
+  },
+  {
+    "revision": "44c415f5915c5fbf1a61",
+    "url": "/reactWeatherApp/static/js/2.d959fda4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactWeatherApp/static/js/2.d959fda4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf37ef3e303fa5a87ae",
+    "url": "/reactWeatherApp/static/js/main.70de1450.chunk.js"
+  },
+  {
+    "revision": "9f553ed7ee8514722f59",
+    "url": "/reactWeatherApp/static/js/runtime-main.889fa658.js"
+  }
+]);
